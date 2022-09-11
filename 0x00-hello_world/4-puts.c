ef21014a4059306b68b2092622e769e6ac377f6a
Brenda <brendabange@gmail.com>
2022-09-11 11:43:33 -0700

    C LLP Task 4.0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+ * prints "Programming is like building a multilingual puzzle" using puts only
+ * returns 0
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
