7f75fd2584f742729f8320828484b4f8bc021cd9
Brenda <brendabange@gmail.com>
2022-09-11 13:08:07 -0700

    C LLP Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print using scanf function
+ *
+ * Description: using the main function
+ * This program prints "and that piece of art is useful - Dora Korpar, 2015-10-19"
+ * Returns: 1
+ */
+int main (void)
+{
+	scanf("and that piece of art is useful - Dora Korpar, 2015-10-19";
+	returns (1);
+}
+
+
