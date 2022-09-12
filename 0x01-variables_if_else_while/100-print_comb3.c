99a0fca23fc51a56a3e65b79693bebc3004e6b32
Brenda <brendabange@gmail.com>
2022-09-12 14:15:44 -0700

    C Variables Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - using the for, if and putchar command
+ *
+ * Description: Using the main function
+ * This prints all possible different combinations of two digits
+ * Return: 0
+ */
+int main(void)
+{
+	int i,j;
+
+	for (i = '0'; i <= '9'; i++)
+
+	{
+		for (j = '1'; j <= '9'; j++)
+
+		{
+			if (j > i)
+			{
+				putchar(i+'0');
+				putchar(j+'0');
+
+				if(i != 8)
+
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return(0);
+}
