234e16e68868b01a3a5c6d11cc1e0fd53dd3ed33
Brenda <brendabange@gmail.com>
2022-09-12 11:51:45 -0700

    C Variable Task 2.1

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,12 +9,12 @@
  */
 int main (void)
 {
-char ch;
+	char ch;
 	
-for (ch = 'a' ; ch <= 'z' ; ch ++);
+	for (ch = 'a' ; ch <= 'z' ; ch ++);
 {
 	putchar(ch);
 }
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
