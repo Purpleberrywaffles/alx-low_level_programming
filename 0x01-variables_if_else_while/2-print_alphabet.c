4318cfb41bda2dd9868bee39e3a09bd1e95ca396
Brenda <brendabange@gmail.com>
2022-09-12 11:39:52 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - using the for and putchar command
+ *
+ * Description: Using the main function
+ * This program prints the alphabet in lowercase
+ * Return: 0
+ */
+int main (void)
+{
+	char ch;
+	
+	for (ch = 'a' ; ch <= 'z' ; ch ++);
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
