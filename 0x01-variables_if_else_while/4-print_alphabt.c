091039c022024de91b68311b598a29d177a184bb
Brenda <brendabange@gmail.com>
2022-09-12 13:08:10 -0700

    C Variable Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - using the putchar command
+ *
+ * Description: using the main command
+ * This program prints alphabets in lowercase
+ */
+int main (void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+	}
+
+	putchar('\n');
+	return(0);
+}
