70cfa592aad47541194b04dc1fe537cde023931a
Brenda <brendabange@gmail.com>
2022-09-12 13:35:05 -0700

    C Variables Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - using for and putchar command
+ *
+ * Description: using the main function
+ * This prints the lowercase alphabets in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char b;
+
+	for (b = 'a'; b <= 'z'; b--)
+	{
+		putchar(b);
+	}
+
+	putchar('\n');
+	return(0);
+}	
