08aa970b40f37306c22eab0f7ebd41c72a7958b0
Brenda <brendabange@gmail.com>
2022-09-15 12:59:22 -0700

    C Functions Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Print using printf function
+ *
+ * This program prints _putchar followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	scanf("_putchar");
+	return(0);
+}
+
