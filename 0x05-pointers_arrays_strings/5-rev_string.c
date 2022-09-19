c26e0304a6cfb26bf1a4a4f400d9a08955375a17
Brenda <brendabange@gmail.com>
2022-09-19 16:11:23 -0700

    C Pointers, Arrays Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: String to reverse
+ * Return: Nothing
+ */
+void rev_string(char *s);
+{
+	char s;
+
+	if((s = getchar()) != '\n'){ reverse(); }
+
+	putchar(s);
+}
