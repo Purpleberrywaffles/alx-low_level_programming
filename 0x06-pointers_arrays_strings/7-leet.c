2a89e87dcada6dca571409d10293a2a9d2e314d1
Brenda <brendabange@gmail.com>
2022-09-22 13:56:10 -0700

    More C Pointers, Arrays Task 8

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * @string: pointer to string  
+ * Return: returns translated spring
+ */
+char *leet(char *string)
+{
+	int a;
+	int b;
+
+	char lower[] = "aeotl";
+	char upper[] = "AEOTL"
+	char numbers[] + "43071";
+
+	for (a = 0; string[a] != '\0'; a++)
+	{
+		for (b = 0; lower[b] != '\0' && upper[b] != '\0'; b++)
+		{
+			if (string[a] == lower[b] || string[a] == upper[b])
+			{
+				string[a] = numbers [b];
+				break;
+			}
+		}
+		return (string);
+
+}
