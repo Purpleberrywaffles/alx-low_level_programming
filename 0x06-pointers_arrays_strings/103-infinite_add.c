5fd558f8b5783433ddb025bfe099c55a4b1777e7
Brenda <brendabange@gmail.com>
2022-09-23 12:31:08 -0700

    More C Pointers, Arrays Task 11.1

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,38 +11,20 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l, m, n;
-	for (1= 0; n1[i]; i++
+	int index, n1_len = 0, n2_len = 0;
 
-	for (j = 0; n2[j]; j++)
+	for (index = 0; *(n1 + index); index++)
+		n1_len++;
+	
+	for (index = 0; *(n2 + index); index++)
+		n2_len++;
 
-	if (i > size_r || j > size_r)
+	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
 		return (0);
 
-	m = 0;
-	for (i -= 1, j -= 1, k =0; k < size_r - 1; i--, j--, k++)
-	{
-		n =m;
-		if (i >= 0)
-			n += n1[i] - '0';
-		if (j >= 0)
-			n += n2[j] - '0';
-		if (i < 0 && j < 0 && n == 0)
-		{
-			break;
-		}
-		m = n / 10;
-		r[k] = n % 10 + '0';
-i	}
-	r[k] = '\0';
-	if (i >= 0 || j >= 0 || m)
-		return (0);
+	n1 += n1_len - 1;
+	n2 += n2_len - 1;
+	*(r + size_r) = '\0';
 
-	for (k -= 1, 1 = 0; 1 < k; k--, l++)
-	{
-		m = r[k];
-		r[k] = r[1];
-		r[1] = m;
-	}
-	return (r);
-i
+	return (add_strings(n1, n2, r, --size_r));
+}
