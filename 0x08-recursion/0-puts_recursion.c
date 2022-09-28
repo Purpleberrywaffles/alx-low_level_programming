14c3fbd1b3f9736b88752f952e0e48c90e708c32
Brenda <brendabange@gmail.com>
2022-09-28 10:28:16 -0700

    C Recursions

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: the string to be printed
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+C Recursions Task 0
