2f1b77ba3cd437ac659b38e33af0f47be202aa48
Brenda <brendabange@gmail.com>
2022-09-28 12:18:18 -0700

    C Recursions Task 6.1

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -22,6 +22,25 @@ int helperfunction(int num, int i)
 	}
 	else
 	{
-		return (i);
+		return (1);
+	}
+}
+
+/**
+ * is_prime_number - checks if number is prime or not
+ * @n: number to be checked
+ *
+ * Return: 1 if number is prime
+ * 0 if number is not prime
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (helperfunction(n, 23));
 	}
 }
