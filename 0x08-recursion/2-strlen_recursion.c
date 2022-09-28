332527efd9c999d4d6e2981571576fd4236d5d14
Brenda <brendabange@gmail.com>
2022-09-28 11:40:20 -0700

    C Recursions Task 2.3

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,17 +2,18 @@
 
 /**
  * _strlen_recursion - Returns the length of a string
- * @s: The string to be measured 
+ * @s: The string to be measured
  *
  * Return: The length of a string
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
-
 	if (*s)
 	{
-		len++;
-		len += _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
 	}
 }
--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,17 +2,18 @@
 
 /**
  * factorial - returns the factorial of a given number
- * @n: number whose factorial is given
+ * @n: number whose factorial is to be found
  *
  * Return: -1 if lower than 0
  */
 int factorial(int n)
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
+	int result = n;
 
-	else 
+	if (n < 0)
+		return (-1);
+
+	else if (n >= 0 && n <= 1)
+	{
 		return (1);
-}
+	}
