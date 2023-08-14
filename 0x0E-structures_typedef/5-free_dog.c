78a79b7284a3ea1fdc1a699a62ead2db67adf1c3
purpleberrywaffles <brendabange@gmail.com>
2023-08-14 13:56:05 -0700

    C Structuers, typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees dog
+ * @d: pointer to struct dog_t
+ *
+ * Return: NULL
+ */
+void free_dog(dog_t *d)
+{
+	if (newer->name == NULL || newer->owner == NULL)
+	{
+
+		free(newer->name);
+		free(newer->owner);
+		free(newer);
+		return (NULL);
+	}
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+typedef struct dog dog_t;
+
+#endif /* DOG_H */
