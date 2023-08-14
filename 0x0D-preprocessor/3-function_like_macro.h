13050bf81d465a49f06828bf786e76e3f2da72a4
purpleberrywaffles <brendabange@gmail.com>
2023-08-14 12:55:13 -0700

    Changes 0

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) (abs(x))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
 #include <stdio.h>
 
 #endif /* FUNCTION_LIKE_MACRO_H */
