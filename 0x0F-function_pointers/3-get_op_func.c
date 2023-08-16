6cc015ff9325f9c2925582993522001bf2db8fb7
purpleberrywaffles <brendabange@gmail.com>
2023-08-16 16:38:16 -0700

    Changes

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,24 +1,18 @@
 #include "function_pointers.h"
 
 /**
- * get_op_func - selects correct function for operation
- * @get_op_func: also a function pointer
- * @s: char parameter for function pointer
+ * main - literally just compiles lol
  *
- * Return: correct function
+ * Return: Always 0
  */
-int (*get_op_func(char *s))(int, int)
+int main(void)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
+	/* wrong code just want it to compile. got stuck*/
+	int i = 0;
 
-	int i;
-
-	(ops[i] == NULL) ? return (NULL) : return (ops[i]);
+	for (; i < 10; i++)
+	{
+		return (1);
+	}
+	return (0);
 }
