2b2ab4973a5c3e414e29fa99a6a8b200d6ae633e
purpleberrywaffles <brendabange@gmail.com>
2023-08-16 16:49:10 -0700

    Changes 2

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -2,6 +2,7 @@
 #define CALC_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct op - Struct op
@@ -20,6 +21,5 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
 
 #endif /* CALC_H */
